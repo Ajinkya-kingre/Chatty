3dff3f63c94b0cfa87ca25462494d6e3efbcc233
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-05-12 11:35:45 +0530

    env

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const connectionDB = require("./db/db");
-const routes = require("./routes/auth/authRoute")
-
-
+const routes = require("./routes/auth/authRoute");
+require("dotenv").config();
+ // remove this after you've confirmed it is working
 
 // user Model
 const userModel = require("./model/user");
@@ -20,21 +20,21 @@ app.get("/", (req, res) => {
   res.send("welcome");
 });
 
-
 //auth routes
-app.use('/api/auth',routes)
+app.use("/api/auth", routes);
 
-
-// 
+//
 // route/router.js
 
 const { authenticate } = require("./middleware/authUserMiddle");
 
 app.route("/protected-route").get(authenticate, (req, res) => {
   // Only authenticated users can access this route
-  res.json({ message: "Protected route accessed successfully", user: req.user });
+  res.json({
+    message: "Protected route accessed successfully",
+    user: req.user,
+  });
 });
-
 
 // localhost
 const PORT = process.env.PORT || 8000;
